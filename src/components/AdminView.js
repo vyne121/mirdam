bb3fde39082470224e01b1b574365a6a6e44723a
vyne121 <vyne121@gmail.com>
2024-01-26 15:30:26 +0100

    HTTPS instead of HTTP

--- src/components/AdminView.js
+++ src/components/AdminView.js
@@ -11,7 +11,7 @@ function AdminView() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                let response = await axios.get('http://127.0.0.1:5000/all');
+                let response = await axios.get('https://127.0.0.1:5000/all');
                 console.log('Type of response.data:', typeof response.data[0]);
                 console.log(response.data);
                 setData(response.data)
--- src/components/FeedbackPage.js
+++ src/components/FeedbackPage.js
@@ -20,7 +20,7 @@ const FeedbackPage = () => {
 
         useEffect(() => {
             const currentId = sessionStorage.getItem("uID");
-            const checkUIdEndpoint = "http://16.171.37.246:5000/family?uID=" + currentId;
+            const checkUIdEndpoint = "https://16.171.37.246:5000/family?uID=" + currentId;
 
             fetch(checkUIdEndpoint)
                 .then(response => response.json())
@@ -60,7 +60,7 @@ const FeedbackPage = () => {
         function checkIdentifier() {
             console.log(identifierFieldValue)
             setErrorMessage("")
-            const checkUIdEndpoint = "http://16.171.37.246:5000/check_uid?uID=" + identifierFieldValue;
+            const checkUIdEndpoint = "https://16.171.37.246:5000/check_uid?uID=" + identifierFieldValue;
             fetch(checkUIdEndpoint)
                 .then(response => response.json())
                 .then(data => {
