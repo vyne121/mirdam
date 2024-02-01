bf7d1fd526e09f21f1ceb79f36ce89484aa9421d
vyne121 <vyne121@gmail.com>
2024-02-01 13:49:57 +0100

    HELMET

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Helmet} from "react-helmet";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Helmet>
+            <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+        </Helmet>
+        <App/>
+    </React.StrictMode>
 );
 
 reportWebVitals();
